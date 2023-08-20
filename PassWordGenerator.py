6f28f0f9d2ed9c985d5af0128da77bce96540a4b
Ali <parsa201909@gmail.com>
2023-08-20 15:20:26 +0330

    version 1

--- PassWordGenerator.py
+++ PassWordGenerator.py
@@ -2,49 +2,28 @@
 import random
 import string
 
-E_num=[]
-E_char=[]
-E_comp=[]
-# Functions
-def random_number():
-    return random.randint(0,9)
-
-def random_char():
-    return random.choice(string.ascii_letters)
-
 # choose your difficulity
-level=input('Choose your password level(simple,medium,high): ')
+level = input('Choose your password level(simple,medium,high): ')
 
 #password character numbers
-char_num=int(input('How many characters does your password have? '))
+char_num = int(input('How many characters does your password have? '))
 
 #generating password
-if level=='simple':
+if level == 'simple':
     try:
-        for y in range(0,char_num):
-            num1=str(random_number())
-            E_num.append(num1)
-        TotalPassWord=''.join(E_num)
+        TotalPassWord = ''.join(random.choices(string.digits, k=char_num))
     except:
         print('Sorry! Something went wrong!')
 
-elif level=='medium':
+elif level == 'medium':
     try:
-        for o in range(0,char_num):
-            char1=random_char()
-            E_char.append(char1)
-        TotalPassWord=''.join(E_char)
+        TotalPassWord = ''.join(random.choices(string.ascii_letters, k=char_num))
     except:
         print('Sorry! Something went wrong!')
     
-elif level=='high':
+elif level == 'high':
     try:
-        for w in range(0,char_num):
-            num2=str(random_number())
-            char2=random_char()
-            chosen=random.choice([num2,char2])
-            E_comp.append(chosen)
-            TotalPassWord=''.join(E_comp)
+        TotalPassWord = ''.join(random.choices(string.ascii_letters + string.digits, k=char_num))
     except:
         print('Sorry! Something went wrong!')
 
